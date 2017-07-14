84898320f6a3e15dca46fe52af3890f446e790dd
NewBeg71 <dfwg4@hotmail.com>
2017-07-14 18:06:20 -0400

    bonus reset button

--- some javascript.js
+++ some javascript.js
@@ -84,9 +84,17 @@ var flipCard = function(){
 		//console.log("User flipped " + cards[cardId].rank); NOT NEEDED ???
 };
 
+
+
+
 //Following two lines no longer needed - was just to show how functions are called using set arguments
 /*flipCard(0);
 flipCard(2);*/
+
+var newSet = function(){
+	var originalState = document.getElementById('game-board').cloneNode();
+	document.getElementById('game-board').replaceWith(originalState);
+};
 
 var createBoard = function(){
 	for (var i = 0; i < cards.length; i++) {
@@ -96,9 +104,109 @@ var createBoard = function(){
 		cardElement.addEventListener ('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
 	}
+	var button = document.getElementById('reset');
+	button.addEventListener('click', newSet);
 }
 
+//need to use MATH.rnd as another for
+
 createBoard();
+
+/*var reset = function(){};
+	ABOVE IS INITIAL WHICH BECAME - HAVE NOT TESTED
+	NEXT FOUR IS WHAT WORKED BUT DID NOT REMOVE PreVIOUS BOARD
+var newSet = function(){
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', newSet);
+createBoard();
+}
+
+put lines 114 & 115 here then simply call createboard function?
+
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+}
+var button = document.getElementById('reset');
+button.addEventListener('click', newSet);*/
+
+/*var resetBackImg = function(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		resetBackImg();
+};*/
+/*var originalState = function(){
+	$("#game-board").clone();
+	$("#game-board").replaceWith(originalState.clone());
+}*/
+
+//$('#regTitle').empty().append(newcontent);
+
+
+/*var originalState = function() {
+	var newBoard = $("#game-board").clone();
+	$("#game-board").empty().append(newBoard);
+	//$("#game-board").remove(cardElement);
+	//$("#game-board").replaceWith(newBoard);
+	//originalState();
+};*/
+
+//var newSet = function(){
+//$(selector).html(function(index,currentcontent))
+//ATTEMPT WITH JQUERY
+
+	/*var originalState = function(){
+		var newBoard = $("#game-board").remove();
+		$("#game-board").clone();
+	//$("#game-board").clone();
+		$("#game-board").replaceWith(newBoard);
+		var button = document.getElementById('reset');
+		button.addEventListener('click', newBoard);
+};*/
+
+	//LETS TRY SOMETHING DIFFERENT
+
+			//$("#game-board").replaceWith(originalState);
+			//newSet();
+		//originalState();
+
+	//var originalState = function() {
+
+		//$("#game-board").empty().append(newBoard);
+
+	//originalState();
+	//};
+
+	//newSet();
+	//originalState();
+//};
+	//NEED TO DELETE PREVIOUS CARDS OR REFLIP THEM TO BACK IMAGE
+	/*var originalState = function(){
+		$("#game-board").clone();
+		$("#game-board").replaceWith(originalState.clone());
+		createBoard();
+	}*/
+
+	/*var resetBackImg = function(){
+		for (var i = 0; i < cards.length; i++) {
+			var cardElement = document.createElement('img');
+			cardElement.setAttribute('src', 'images/back.png');
+
+			//resetBackImg();
+	};*/
+	//resetBackImg();*/
+	//var cardsInPlay = [];
+
+	//checkMatch();
+	//createBoard();
+	//checkMatch();
+	//newSet();
+
+
+//newSet();
+
+/*var button = document.getElementById('reset');
+button.addEventListener('click', newSet);*/
 
 //lines 53-57 where for learning purposes
 /*var cardOne = cards[0];
@@ -106,9 +214,6 @@ cardsInPlay.push(cardOne);
 
 var cardTwo = cards [2];
 cardsInPlay.push(cardTwo);*/
-
-
-
 
 
 
